f90d54aab45d0deb282c3250fbf1acc321bdc923
Sammy-ch <sammystcherubin@gmail.com>
2025-05-27 20:28:19 +0200

    implemented channels for parsing input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use rayon::prelude::*;
 use std::env;
+use std::thread;
+
+use crossbeam::channel::unbounded;
 
 use svg::Document;
 use svg::node::element::path::{Command, Data, Position};
@@ -16,6 +19,11 @@ const HOME_Y: isize = HEIGHT / 2;
 const HOME_X: isize = WIDTH / 2;
 
 const STROKE_WIDTH: usize = 5;
+
+enum Work {
+    Task((usize, u8)),
+    Finished,
+}
 
 #[derive(Debug, Clone, Copy)]
 enum Orientation {
@@ -101,21 +109,57 @@ impl Artist {
     }
 }
 
+fn parse_byte(byte: u8) -> Operation {
+    match byte {
+        b'0' => Home,
+        b'1'..=b'9' => {
+            let distance = (byte - 0x30) as isize;
+            Forward(distance * (HEIGHT / 10))
+        }
+        b'a' | b'b' | b'c' => TurnLeft,
+        b'd' | b'e' | b'f' => TurnRight,
+        _ => Noop(byte),
+    }
+}
+
 fn parse(input: &str) -> Vec<Operation> {
-    input
-        .as_bytes()
-        .par_iter()
-        .map(|byte| match byte {
-            b'0' => Home,
-            b'1'..=b'9' => {
-                let distance = (byte - 0x30) as isize;
-                Forward(distance * (HEIGHT / 10))
+    let n_threads = 2;
+    let (todo_tx, todo_rx) = unbounded();
+    let (results_tx, results_rx) = unbounded();
+
+    let mut n_bytes = 0;
+    for (i, byte) in input.bytes().enumerate() {
+        todo_tx.send(Work::Task((i, byte))).unwrap();
+        n_bytes += 1;
+    }
+
+    for _ in 0..n_threads {
+        todo_tx.send(Work::Finished).unwrap();
+    }
+
+    for _ in 0..n_threads {
+        let todo = todo_rx.clone();
+        let results = results_tx.clone();
+
+        thread::spawn(move || {
+            loop {
+                let task = todo.recv();
+                let result = match task {
+                    Err(_) => break,
+                    Ok(Work::Finished) => break,
+                    Ok(Work::Task((i, byte))) => (i, parse_byte(byte)),
+                };
+                results.send(result).unwrap();
             }
-            b'a' | b'b' | b'c' => TurnLeft,
-            b'd' | b'e' | b'f' => TurnRight,
-            _ => Noop(*byte),
-        })
-        .collect()
+        });
+    }
+
+    let mut ops = vec![Noop(0); n_bytes];
+    for _ in 0..n_bytes {
+        let (i, op) = results_rx.recv().unwrap();
+        ops[i] = op;
+    }
+    ops
 }
 
 fn convert(operations: &Vec<Operation>) -> Vec<Command> {
